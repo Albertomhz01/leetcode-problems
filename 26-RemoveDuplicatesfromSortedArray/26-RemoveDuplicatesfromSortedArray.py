2079cb611b08be16c4d57492829bb2aaa5ac7f62
Alberto Mtz <135084981+Albertomhz01@users.noreply.github.com>
2026-04-15 21:19:03 -0600

    File updated at 4/15/2026, 9:19:03 PM

--- 26-RemoveDuplicatesfromSortedArray/26-RemoveDuplicatesfromSortedArray.py
+++ 26-RemoveDuplicatesfromSortedArray/26-RemoveDuplicatesfromSortedArray.py
@@ -1,17 +1,14 @@
-# Last updated: 4/15/2026, 9:18:16 PM
+# Last updated: 4/15/2026, 9:19:03 PM
 1class Solution:
 2    def removeDuplicates(self, nums: List[int]) -> int:
 3        l1 = 0
 4        l2 = 1
-5        print(len(nums)-1)
-6        print(nums)
-7
-8        while l2 <= len(nums)-1:
-9            if nums[l1] == nums[l2]:
-10                print(f"same numbers: {nums[l1]}, {nums[l2]}")
-11                l2 += 1
-12            else:
-13                nums[l1+1] = nums[l2]
-14                l1 += 1
-15                l2 += 1
-16        return l1+1
+5
+6        while l2 <= len(nums)-1:
+7            if nums[l1] == nums[l2]:
+8                l2 += 1
+9            else:
+10                nums[l1+1] = nums[l2]
+11                l1 += 1
+12                l2 += 1
+13        return l1+1
