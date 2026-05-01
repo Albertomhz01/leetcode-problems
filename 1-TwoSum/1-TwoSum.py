5d354f5597d82f9af2a939f10845d42595c85966
Alberto Mtz <135084981+Albertomhz01@users.noreply.github.com>
2026-04-30 21:50:18 -0600

    File updated at 4/30/2026, 9:50:18 PM

--- 1-TwoSum/1-TwoSum.py
+++ 1-TwoSum/1-TwoSum.py
@@ -1,17 +1,17 @@
-# Last updated: 4/12/2026, 10:23:24 PM
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-
-        ii = 0
-        jj = ii+1
-        print(len(nums))
-        print(nums)
-
-        while ii <= len(nums)-2:
-            if nums[ii] + nums[jj] == target:
-                return [ii, jj]
-            elif jj < len(nums)-1:
-                jj += 1
-            else:
-                ii += 1
-                jj = ii+1
+# Last updated: 4/30/2026, 9:50:18 PM
+1class Solution(object):
+2    def twoSum(self, nums, target):
+3        """
+4        :type nums: List[int]
+5        :type target: int
+6        :rtype: List[int]
+7        """
+8        prevMap = {} # val : index
+9
+10        for ii, n in enumerate(nums):
+11            diff = target - n
+12            if diff in prevMap:
+13                return [prevMap[diff], ii]
+14            prevMap[n] = ii
+15        return
+16        
