ce7a39e198cb0c7b3f832794296762ef33bceb51
Alberto Mtz <135084981+Albertomhz01@users.noreply.github.com>
2026-04-30 23:34:00 -0600

    File updated at 4/30/2026, 11:33:59 PM

--- 1-TwoSum/1-TwoSum.py
+++ 1-TwoSum/1-TwoSum.py
@@ -1,35 +1,11 @@
-# Last updated: 4/30/2026, 11:15:23 PM
-1from collections import deque
-2class Solution:
-3    def numIslands(self, grid: List[List[str]]) -> int:
-4        if not grid:
-5            return 0
-6
-7        rows, cols = len(grid), len(grid[0])
-8        visit = set()
-9        islands = 0
-10
-11        def bfs(r, c):
-12            q = deque()
-13            visit.add((r, c))
-14            q.append((r, c))
-15
-16            while q:
-17                row, col = q.popleft()
-18                directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
-19
-20                for dr, dc in directions:
-21                    r, c = row + dr, col + dc
-22                    if (r in range(rows) and
-23                        c in range(cols) and
-24                        grid[r][c] == "1" and
-25                        (r, c) not in visit):
-26                        q.append((r, c))
-27                        visit.add((r, c))
-28
-29        for r in range(rows):
-30            for c in range(cols):
-31                if grid[r][c] == "1" and (r, c) not in visit:
-32                    bfs(r, c)
-33                    islands += 1
-34        return islands
+# Last updated: 4/30/2026, 11:33:59 PM
+1class Solution:
+2    def twoSum(self, nums: List[int], target: int) -> List[int]:
+3        prevMap = {} # val : index
+4
+5        for ii, n in enumerate(nums):
+6            diff = target - n
+7            if diff in prevMap:
+8                return [prevMap[diff], ii]
+9            prevMap[n] = ii
+10        return
