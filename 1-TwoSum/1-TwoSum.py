f97909aee2d42417420e41eb19d18e1635cd57d5
Alberto Mtz <135084981+Albertomhz01@users.noreply.github.com>
2026-05-10 21:24:52 -0600

    File updated at 5/10/2026, 9:24:52 PM

--- 1-TwoSum/1-TwoSum.py
+++ 1-TwoSum/1-TwoSum.py
@@ -1,11 +1,14 @@
-# Last updated: 4/30/2026, 11:33:59 PM
+# Last updated: 5/10/2026, 9:24:52 PM
 1class Solution:
-2    def twoSum(self, nums: List[int], target: int) -> List[int]:
-3        prevMap = {} # val : index
-4
-5        for ii, n in enumerate(nums):
-6            diff = target - n
-7            if diff in prevMap:
-8                return [prevMap[diff], ii]
-9            prevMap[n] = ii
-10        return
+2    def isPalindrome(self, x: int) -> bool:
+3        x = str(x)
+4        left = 0
+5        right = len(x)-1
+6
+7        while left < len(x)//2:
+8            if x[left] == x[right]:
+9                left += 1
+10                right -= 1
+11            else:
+12                return False
+13        return True
