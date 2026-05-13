08dba4c134c2ac943ff2ce1363a62bc7132de45d
Alberto Mtz <135084981+Albertomhz01@users.noreply.github.com>
2026-05-12 21:24:19 -0600

    Create 877-StoneGame

---
+++ 877-StoneGame/877-StoneGame.py
@@ -0,0 +1,7 @@
+# Last updated: 5/12/2026, 9:24:19 PM
+1from collections import deque
+2import random
+3class Solution():
+4    def stoneGame(self, piles: List[int]) -> bool:
+5        return True
+6        
