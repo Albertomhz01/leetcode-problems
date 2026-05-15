085da13d40dbf2ed1801dcae88f0b2619d47aaa0
Alberto Mtz <135084981+Albertomhz01@users.noreply.github.com>
2026-05-14 18:14:17 -0600

    File updated at 5/14/2026, 6:14:17 PM

--- 1-TwoSum/1-TwoSum.py
+++ 1-TwoSum/1-TwoSum.py
@@ -1,17 +1,22 @@
-# Last updated: 5/11/2026, 7:28:36 PM
-1# Definition for singly-linked list.
-2# class ListNode:
-3#     def __init__(self, val=0, next=None):
-4#         self.val = val
-5#         self.next = next
-6class Solution:
-7    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-8        prev, curr = None, head
-9
-10        while curr:
-11            nxt = curr.next
-12            curr.next = prev
-13            prev = curr
-14            curr = nxt
-15        return prev
-16        
+# Last updated: 5/14/2026, 6:14:17 PM
+1class Solution:
+2    def longestCommonPrefix(self, strs: list[str]) -> str:
+3        s_min = min(strs, key=len)
+4        ii = 0
+5        jj = 0
+6        res = ""
+7        stop = False
+8        for s in range(len(s_min)): # len(strs[0]) = 4, flow
+9            # print(s_min[s]) f->l->o->w
+10            for ii in range(len(strs)):
+11                # print(strs[ii][s], s_min[s])
+12                if strs[ii][s] != s_min[s]:
+13                    stop = True
+14                    break
+15                else:
+16                    continue
+17            if stop:
+18                break
+19            res += s_min[s]
+20        
+21        return res
