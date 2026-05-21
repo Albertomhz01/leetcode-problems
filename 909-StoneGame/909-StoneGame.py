7938e308ad576ff1a4a8a01f38cf125b69d80769
Alberto Mtz <135084981+Albertomhz01@users.noreply.github.com>
2026-05-21 10:39:15 -0600

    Create 909-StoneGame

---
+++ 909-StoneGame/909-StoneGame.py
@@ -0,0 +1,7 @@
+# Last updated: 5/21/2026, 10:39:15 AM
+from collections import deque
+import random
+class Solution():
+    def stoneGame(self, piles: List[int]) -> bool:
+        return True
+        
