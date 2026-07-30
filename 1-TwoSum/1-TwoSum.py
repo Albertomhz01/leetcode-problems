ab44bbaa040761f21f6f6ef8321897ed25b7fd8f
Alberto Mtz <135084981+Albertomhz01@users.noreply.github.com>
2026-07-30 15:46:23 -0600

    File updated at 7/30/2026, 3:46:28 PM

--- 1-TwoSum/1-TwoSum.py
+++ 1-TwoSum/1-TwoSum.py
@@ -1,22 +1,11 @@
-# Last updated: 5/14/2026, 6:14:17 PM
+# Last updated: 7/30/2026, 3:46:28 PM
 1class Solution:
-2    def longestCommonPrefix(self, strs: list[str]) -> str:
-3        s_min = min(strs, key=len)
-4        ii = 0
-5        jj = 0
-6        res = ""
-7        stop = False
-8        for s in range(len(s_min)): # len(strs[0]) = 4, flow
-9            # print(s_min[s]) f->l->o->w
-10            for ii in range(len(strs)):
-11                # print(strs[ii][s], s_min[s])
-12                if strs[ii][s] != s_min[s]:
-13                    stop = True
-14                    break
-15                else:
-16                    continue
-17            if stop:
-18                break
-19            res += s_min[s]
-20        
-21        return res
+2    def twoSum(self, nums: List[int], target: int) -> List[int]:
+3        prevMap = {} # val : index
+4
+5        for ii, n in enumerate(nums):
+6            diff = target - n
+7            if diff in prevMap:
+8                return [prevMap[diff], ii]
+9            prevMap[n] = ii
+10        return
