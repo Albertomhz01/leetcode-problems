843c24702bfee8525cd2e5c37a92cf30a63c9aed
Alberto Mtz <135084981+Albertomhz01@users.noreply.github.com>
2026-07-30 15:51:09 -0600

    File updated at 7/30/2026, 3:51:14 PM

--- 14-LongestCommonPrefix/14-LongestCommonPrefix.py
+++ 14-LongestCommonPrefix/14-LongestCommonPrefix.py
@@ -1,22 +1,22 @@
-# Last updated: 4/12/2026, 10:23:16 PM
-class Solution:
-    def longestCommonPrefix(self, strs: list[str]) -> str:
-        s_min = min(strs, key=len)
-        ii = 0
-        jj = 0
-        res = ""
-        stop = False
-        for s in range(len(s_min)): # len(strs[0]) = 4, flow
-            # print(s_min[s]) f->l->o->w
-            for ii in range(len(strs)):
-                print(strs[ii][s], s_min[s])
-                if strs[ii][s] != s_min[s]:
-                    stop = True
-                    break
-                else:
-                    continue
-            if stop:
-                break
-            res += s_min[s]
-        
-        return res
+# Last updated: 7/30/2026, 3:51:14 PM
+1class Solution:
+2    def longestCommonPrefix(self, strs: list[str]) -> str:
+3        s_min = min(strs, key=len)
+4        ii = 0
+5        jj = 0
+6        res = ""
+7        stop = False
+8        for s in range(len(s_min)): # len(strs[0]) = 4, flow
+9            # print(s_min[s]) f->l->o->w
+10            for ii in range(len(strs)):
+11                # print(strs[ii][s], s_min[s])
+12                if strs[ii][s] != s_min[s]:
+13                    stop = True
+14                    break
+15                else:
+16                    continue
+17            if stop:
+18                break
+19            res += s_min[s]
+20        
+21        return res
