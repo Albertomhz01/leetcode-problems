fae885af66ee3940e47184d058667dfd6b9a398a
Alberto Mtz <135084981+Albertomhz01@users.noreply.github.com>
2026-07-30 15:58:09 -0600

    File updated at 7/30/2026, 3:58:14 PM

--- 9-PalindromeNumber/9-PalindromeNumber.py
+++ 9-PalindromeNumber/9-PalindromeNumber.py
@@ -1,10 +1,14 @@
-# Last updated: 4/12/2026, 10:23:18 PM
-class Solution:
-    def isPalindrome(self, x: int) -> bool:
-        
-        x = list(str(x))
-
-        if x[:] == x[::-1]:
-            return True
-        else:
-            return False
+# Last updated: 7/30/2026, 3:58:14 PM
+1class Solution:
+2    def isPalindrome(self, x: int) -> bool:
+3        x = str(x)
+4        left = 0
+5        right = len(x)-1
+6
+7        while left < len(x)//2:
+8            if x[left] == x[right]:
+9                left += 1
+10                right -= 1
+11            else:
+12                return False
+13        return True
