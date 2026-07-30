c72578abda2d302286f25c7aead5b0f54c1cbc75
Alberto Mtz <135084981+Albertomhz01@users.noreply.github.com>
2026-07-30 15:52:19 -0600

    File updated at 7/30/2026, 3:52:23 PM

--- 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.py
+++ 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.py
@@ -1,27 +1,27 @@
-# Last updated: 4/12/2026, 10:23:21 PM
-class Solution:
-    def longestPalindrome(self, s: str) -> str:
-        if not s:
-            return None
-        
-        start = 0
-        end = 0
-        
-        def expand(l, r):
-            while l >= 0 and r < len(s) and s[l] == s[r]:
-                l -= 1
-                r += 1
-            return l + 1, r - 1
-        
-        for i in range(len(s)):
-            # odd length
-            l1, r1 = expand(i, i)
-            # even length
-            l2, r2 = expand(i, i + 1)
-            
-            if r1 - l1 > end - start:
-                start, end = l1, r1
-            if r2 - l2 > end - start:
-                start, end = l2, r2
-        
-        return s[start:end + 1]
+# Last updated: 7/30/2026, 3:52:23 PM
+1class Solution:
+2    def longestPalindrome(self, s: str) -> str:
+3        if not s:
+4            return None
+5        
+6        start = 0
+7        end = 0
+8        
+9        def expand(l, r):
+10            while l >= 0 and r < len(s) and s[l] == s[r]:
+11                l -= 1
+12                r += 1
+13            return l + 1, r - 1
+14        
+15        for i in range(len(s)):
+16            # odd length
+17            l1, r1 = expand(i, i)
+18            # even length
+19            l2, r2 = expand(i, i + 1)
+20            
+21            if r1 - l1 > end - start:
+22                start, end = l1, r1
+23            if r2 - l2 > end - start:
+24                start, end = l2, r2
+25        
+26        return s[start:end + 1]
