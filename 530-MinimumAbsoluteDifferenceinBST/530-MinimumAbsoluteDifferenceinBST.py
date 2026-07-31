870afc669ee16c1148afee48e252bf4846de700c
Alberto Mtz <135084981+Albertomhz01@users.noreply.github.com>
2026-07-31 14:48:11 -0600

    File updated at 7/31/2026, 2:48:16 PM

--- 530-MinimumAbsoluteDifferenceinBST/530-MinimumAbsoluteDifferenceinBST.py
+++ 530-MinimumAbsoluteDifferenceinBST/530-MinimumAbsoluteDifferenceinBST.py
@@ -1,4 +1,4 @@
-# Last updated: 7/31/2026, 2:37:02 PM
+# Last updated: 7/31/2026, 2:48:16 PM
 1# Definition for a binary tree node.
 2# class TreeNode:
 3#     def __init__(self, val=0, left=None, right=None):
@@ -7,20 +7,17 @@
 6#         self.right = right
 7class Solution:
 8    def getMinimumDifference(self, root: Optional[TreeNode]) -> int:
-9        vals = []
-10        mini_val = float('inf')
-11
-12        def dfs(node):
-13            if not node:
-14                return
-15            vals.append(node.val)
-16            dfs(node.left)
-17            dfs(node.right)
-18
-19        dfs(root)
-20
-21        for ii in range(len(vals)):
-22            for kk in range(ii+1, len(vals)):
-23                if abs(vals[ii] - vals[kk]) < mini_val:
-24                    mini_val = abs(vals[ii]-vals[kk])
-25        return mini_val
+9        prev, res = None, float("inf")
+10
+11        def dfs(node):
+12            if not node:
+13                return
+14            dfs(node.left)
+15            nonlocal prev, res
+16            if prev:
+17                res = min(res, node.val - prev.val)
+18            prev = node
+19            dfs(node.right)
+20        
+21        dfs(root)
+22        return res
