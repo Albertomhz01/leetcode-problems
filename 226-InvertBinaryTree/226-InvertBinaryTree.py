b48cf1686356cb5c470f507302824fffec9ab6aa
Alberto Mtz <135084981+Albertomhz01@users.noreply.github.com>
2026-08-03 17:01:45 -0600

    File updated at 8/3/2026, 5:01:49 PM

--- 226-InvertBinaryTree/226-InvertBinaryTree.py
+++ 226-InvertBinaryTree/226-InvertBinaryTree.py
@@ -1,25 +1,21 @@
-# Last updated: 8/2/2026, 9:20:48 PM
+# Last updated: 8/3/2026, 5:01:49 PM
 1# Definition for a binary tree node.
 2# class TreeNode:
 3#     def __init__(self, val=0, left=None, right=None):
 4#         self.val = val
 5#         self.left = left
 6#         self.right = right
-7from collections import deque
-8class Solution:
-9    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-10        if not root:
-11            return
-12
-13        # print(f"root: {root.val}")
-14        # root.left, root.right = root.right, root.left
-15        
-16        def DFS(node):
-17            if not node:
-18                return
-19            node.left, node.right = node.right, node.left
-20            DFS(node.left)
-21            DFS(node.right)
-22        
-23        DFS(root)
-24        return root
+7class Solution:
+8    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+9        def DFS(left_root, right_root):
+10            if not left_root and not right_root:
+11                return True
+12            if not left_root or not right_root:
+13                return False
+14            if left_root.val == right_root.val:
+15                return DFS(left_root.left, right_root.right) and DFS(left_root.right, right_root.left)
+16            else:
+17                return False
+18            
+19        return DFS(root.left, root.right)
+20        
