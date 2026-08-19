8fa4cf956e80da0f66639d60efd615ae6a9e3e80
Alberto Mtz <135084981+Albertomhz01@users.noreply.github.com>
2026-08-19 00:10:44 -0600

    File updated at 8/19/2026, 12:10:50 AM

--- 290-WordPattern/290-WordPattern.py
+++ 290-WordPattern/290-WordPattern.py
@@ -1,33 +1,26 @@
-# Last updated: 8/19/2026, 12:07:03 AM
+# Last updated: 8/19/2026, 12:10:50 AM
 1class Solution:
 2    def wordPattern(self, pattern: str, s: str) -> bool:
-3        map = {}
-4        s_list = s.split()
-5        pattern_list = list()
-6        kk = 0
+3        s_list = s.split()
+4
+5        if len(pattern) != len(s_list):
+6            return False
 7
-8        for word in pattern:
-9            pattern_list.append(word)
+8        map = {}
+9        used = set()
 10
-11        if len(pattern_list) > len(s_list):
-12            return False
-13
-14        for ii in pattern:
-15            map[ii] = s_list[kk]
-16            print(map[ii], s_list[kk])
-17            kk += 1
-18
-19        has_duplicates = len(map) != len(set(map.values()))
-20        if has_duplicates == True:
-21            return False
-22
-23        reword = ""
-24        for ii in pattern_list:
-25            reword += " "
-26            reword += map[ii]
-27
-28        reword = reword.strip()
-29        if reword == s:
-30            return True
-31        else:
-32            return False
+11        for ii in range(len(pattern)):
+12            char = pattern[ii]
+13            word = s_list[ii]
+14
+15            if char in map:
+16                if map[char] != word:
+17                    return False
+18            else:
+19                if word in used:
+20                    return False
+21
+22                map[char] = word
+23                used.add(word)
+24
+25        return True
