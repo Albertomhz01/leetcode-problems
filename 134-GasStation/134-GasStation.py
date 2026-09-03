f5fcfbd83d15f8f629f8650e469139a33d8fd413
Alberto Mtz <135084981+Albertomhz01@users.noreply.github.com>
2026-09-02 22:07:30 -0600

    File updated at 9/2/2026, 10:07:37 PM

--- 134-GasStation/134-GasStation.py
+++ 134-GasStation/134-GasStation.py
@@ -1,19 +1,17 @@
-# Last updated: 7/10/2026, 2:16:02 AM
+# Last updated: 9/2/2026, 10:07:37 PM
 1class Solution:
-2    def isPalindrome(self, s: str) -> bool:
-3        s = "".join(char for char in s if char.isalnum())
-4        s = s.lower()
-5        
-6        if len(s) < 1:
-7            return True
+2    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+3        if sum(gas) < sum(cost):
+4            return -1
+5
+6        tank = 0
+7        start = 0
 8
-9        left = 0
-10        right = len(s) - 1
+9        for i in range(len(gas)):
+10            tank += gas[i] - cost[i]
 11
-12        while left < (len(s))//2:
-13            if s[left] == s[right]:
-14                left += 1
-15                right -= 1
-16            else:
-17                return False
-18        return True
+12            if tank < 0:
+13                start = i + 1
+14                tank = 0
+15
+16        return start
