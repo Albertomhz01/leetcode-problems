5d43c6c99c1e2e61a16023eba93b9a74e96d7644
Alberto Mtz <135084981+Albertomhz01@users.noreply.github.com>
2026-09-05 13:19:50 -0600

    File updated at 9/5/2026, 1:19:57 PM

--- 1-TwoSum/1-TwoSum.py
+++ 1-TwoSum/1-TwoSum.py
@@ -1,11 +1,12 @@
-# Last updated: 7/30/2026, 3:46:28 PM
+# Last updated: 9/5/2026, 1:19:57 PM
 1class Solution:
-2    def twoSum(self, nums: List[int], target: int) -> List[int]:
-3        prevMap = {} # val : index
+2    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+3        map = {}
 4
-5        for ii, n in enumerate(nums):
+5        for ii, n in enumerate(numbers):
 6            diff = target - n
-7            if diff in prevMap:
-8                return [prevMap[diff], ii]
-9            prevMap[n] = ii
+7            if diff in map:
+8                return [map[diff]+1, ii+1]
+9            map[n] = ii
 10        return
+11        
